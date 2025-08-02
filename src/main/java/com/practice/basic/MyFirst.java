4e2e1f0860068f0f770c2e6ad42df7f70edb7b89
Akshith Ikkurthy <akshithyeet@gmail.com>
2025-08-02 20:10:25 +0530

    Initial commit

---
+++ src/main/java/com/practice/basic/MyFirst.java
@@ -0,0 +1,7 @@
+package com.practice.basic;
+
+public class MyFirst {
+    public static void main(String... args){
+        System.out.println("My First Program");
+    }
+}
---
+++ src/main/java/com/practice/basic/SwapNumbers.java
@@ -0,0 +1,13 @@
+package com.practice.basic;
+
+public class SwapNumbers {
+    public static void main(String... args){
+        int a=10;
+        int b=20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+}
